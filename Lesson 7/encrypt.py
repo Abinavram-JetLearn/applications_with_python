a046657d3da991f8d32a59f3b956669fe4803e69
Abinavram <abinavram@hotmail.com>
2026-05-17 15:27:21 +0100

    Add files via upload

--- Lesson 7/encrypt.py
+++ Lesson 7/encrypt.py
@@ -26,17 +26,23 @@ def Cypher():
                 encrypted += chr((ord(char) - 97 + shift) % 26 + 97)
         else:
             encrypted += char
+    with open("encrypted_text.txt", "w") as file:
+        file.write(encrypted)
+        file.close()
+
 
 title = Label(screen, text="Caesar Cypher", bg=BG, fg=UnSat_Text , font=("verdana", 30, "bold"))
 EntryTextLabel = Label(screen, text="Paste or type the sentence you want to cypher", bg=BG, fg=UnSat_Text , font=("verdana", 14, "bold"))
 EntryTextBox = Entry(screen, bg=BG, fg=CARD , font=("verdana", 28, "bold"))
 EntryShiftLabel = Label(screen, text="Shift the slider to make a cypher", bg=BG, fg=UnSat_Text , font=("verdana", 14, "bold"))
 EntryShiftScale = Scale(screen, from_=1, to=25, orient = 'horizontal', bg= CARD, fg= "white", font=("Segoe UI", 10), length= 350)
+SubmitButton = Button(screen, bg=BG, fg=UnSat_Text , font=("verdana", 28, "bold"), text="Submit", command= Cypher)
 
 title.pack(padx = 10, pady = 20)
 EntryTextLabel.pack(padx = 10)
 EntryTextBox.pack(padx = 10, pady = 20)
 EntryShiftLabel.pack(padx = 10)
 EntryShiftScale.pack(padx = 10, pady = 20)
+SubmitButton.pack(padx= 10, pady= 20)
 
 screen.mainloop()
